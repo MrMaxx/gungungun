5d752f6a645e582d693cd0bedb265132e9ad93fd
Maximilian Höflich <maximilian.hoeflich@googlemail.com>
2015-04-05 19:52:52 +0200

    added logging for processing of open fights

--- src/main/java/de/overwatch/gungungun/service/FightService.java
+++ src/main/java/de/overwatch/gungungun/service/FightService.java
@@ -7,15 +7,15 @@ import de.overwatch.gungungun.domain.Party;
 import de.overwatch.gungungun.game.GameEngine;
 import de.overwatch.gungungun.game.GameState;
 import de.overwatch.gungungun.game.GameStateFactory;
-import de.overwatch.gungungun.game.gameevent.GameEvent;
 import de.overwatch.gungungun.repository.FightRepository;
 import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.inject.Inject;
 import java.util.Collection;
-import java.util.List;
 
 @Service
 @Transactional
@@ -29,26 +29,31 @@ public class FightService {
     @Inject
     private GameEngine gameEngine;
 
+    private final Logger log = LoggerFactory.getLogger(FightService.class);
+
     public void processOpenFights(){
         Gson gson = new GsonBuilder().create();
         Collection<Fight> openFights = fightRepository.findOpenFights();
 
         for(Fight fight: openFights){
-
-            GameState gameState = gameStateFactory.createGameState(fight);
-            gameEngine.calculateGame(gameState);
-            String eventJson = gson.toJson(gameState.getGameEvents());
-            fight.setResultingEvents(eventJson);
-            fight.setProcessedAt(new DateTime());
-            if(gameState.getWinningPartyId() != null){
-                Party winner = null;
-                for(Party party: fight.getParticipatingPartys()){
-                    if(party.getId().equals(gameState.getWinningPartyId())){
-                        winner = party;
-                        break;
+            try {
+                GameState gameState = gameStateFactory.createGameState(fight);
+                gameEngine.calculateGame(gameState);
+                String eventJson = gson.toJson(gameState.getGameEvents());
+                fight.setResultingEvents(eventJson);
+                fight.setProcessedAt(new DateTime());
+                if (gameState.getWinningPartyId() != null) {
+                    Party winner = null;
+                    for (Party party : fight.getParticipatingPartys()) {
+                        if (party.getId().equals(gameState.getWinningPartyId())) {
+                            winner = party;
+                            break;
+                        }
                     }
+                    fight.setWinner(winner);
                 }
-                fight.setWinner(winner);
+            }catch(Exception e){
+                log.error("Tried to process Fight with id = "+fight.getId(), e);
             }
 
         }
